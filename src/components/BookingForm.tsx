4ad27086de06a9a2dd086c901dd3f53dfafbca23
Ricardo Ribeiro <ricardoribeiropro4@gmail.com>
2025-10-23 23:42:57 -0300

    atualizando horarios de funcionamento

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -17,7 +17,7 @@ interface BookingFormProps {
 }
 
 const AVAILABLE_HOURS = [
-  '08:00', '09:00', '10:00', '11:00', '13:00', '14:00', '15:00', '16:00', '17:00'
+  '08:00', '09:00', '10:00', '11:00', '13:00', '14:00', '15:00', '16:00', '16:30'
 ];
 
 const BookingForm = ({ onBookingCreated }: BookingFormProps) => {
